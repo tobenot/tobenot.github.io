c70fb0d25f2d5f5f6060a9c1dd00646feab4e10a
tobenot <tobenot@users.noreply.github.com>
2026-06-03 14:29:49 +0000

    deploy: tobenot/Sanctuary-Of-Writers@1e07bad178a8e8af03da00a70a7247ec03631ade

--- js/hitokoto.js
+++
@@ -1,1 +0,0 @@
-fetch("https://v1.hitokoto.cn/?c=k").then(function(t){return t.json()}).then(function(t){document.getElementById("quote").innerText=t.hitokoto}).catch(function(t){console.error(t)});
